1b3c1ab252c6c9e47e762b917fe27523e26cd68a
SupinePandora43 <supinepandoraggg@gmail.com>
2021-02-05 21:40:58 +0500

    100% ulstring?

--- Ultralight.Test/Program.cs
+++ Ultralight.Test/Program.cs
@@ -2,16 +2,18 @@ using System;
 
 namespace Ultralight.Test
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-			var cfg = Ultralight.CreateConfig();
-			var str = Ultralight.CreateString("./resources");
-			Ultralight.ConfigSetResourcePath(cfg, str);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//var cfg = Ultralight.CreateConfig();
+			//var str = Ultralight.CreateString("./resources");
+			//Ultralight.ConfigSetResourcePath(ref cfg, str);
 
-			Console.WriteLine(Ultralight.GetVersionStringSafe());
-			Console.WriteLine(Ultralight.GetVersionStringMarshal());
+			//Console.WriteLine(Ultralight.GetVersionStringSafe());
+			//Console.WriteLine(Ultralight.GetVersionStringMarshal());
+
+			var cfg = new Config() { ResourcePath = (ULString)"./resources"};
 		}
-    }
+	}
 }
--- Ultralight/Config.cs
+++ Ultralight/Config.cs
@@ -1,7 +1,39 @@
+using System;
+using System.Runtime.InteropServices;
+
 namespace Ultralight
 {
-	public struct Config
+	public class Config
 	{
+		public readonly IntPtr ptr;
 
+		public Config()
+		{
+			ptr = Methods.CreateConfig();
+		}
+		~Config()
+		{
+			Methods.ulDestroyConfig(ptr);
+		}
+
+		public ULString ResourcePath
+		{
+			set
+			{
+				Methods.ulConfigSetResourcePath(ptr, value);
+			}
+		}
+		public static implicit operator IntPtr(Config config) => config.ptr;
+		public static class Methods
+		{
+			[DllImport("Ultralight", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ulCreateConfig", ExactSpelling = true)]
+			public static extern IntPtr CreateConfig();
+
+			[DllImport("Ultralight", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+			public static extern void ulDestroyConfig(IntPtr config);
+
+			[DllImport("Ultralight", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+			public static extern void ulConfigSetResourcePath(IntPtr config, IntPtr resource_path);
+		}
 	}
 }
