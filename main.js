d37df255e4f37fa661bd6b4f756a75640cd020da
SupinePandora43 <supinepandoraggg@gmail.com>
2021-03-14 10:15:59 +0000

    Update gh-pages to output generated at 4c9b281

--- main.js
+++ main.js
@@ -225,8 +225,8 @@ var assemblies = [
     "name": "UltralightNet",
     "classes": [
       { "name": "UltralightNet.Methods", "rp": "UltralightNet_Methods.html", "cl": 30, "ucl": 136, "cal": 166, "tl": 0, "ct": "LineCoverage", "mc": "-", "cb": 2, "tb": 30, "lch": [], "bch": [], "hc": [] },
-      { "name": "UltralightNet.ULConfig", "rp": "UltralightNet_ULConfig.html", "cl": 4, "ucl": 13, "cal": 17, "tl": 118, "ct": "LineCoverage", "mc": "-", "cb": 0, "tb": 2, "lch": [], "bch": [], "hc": [] },
-      { "name": "UltralightNet.ULString", "rp": "UltralightNet_ULString.html", "cl": 21, "ucl": 9, "cal": 30, "tl": 126, "ct": "LineCoverage", "mc": "-", "cb": 10, "tb": 18, "lch": [], "bch": [], "hc": [] },
+      { "name": "UltralightNet.ULConfig", "rp": "UltralightNet_ULConfig.html", "cl": 5, "ucl": 19, "cal": 24, "tl": 160, "ct": "LineCoverage", "mc": "-", "cb": 0, "tb": 2, "lch": [], "bch": [], "hc": [] },
+      { "name": "UltralightNet.ULString", "rp": "UltralightNet_ULString.html", "cl": 21, "ucl": 9, "cal": 30, "tl": 127, "ct": "LineCoverage", "mc": "-", "cb": 10, "tb": 18, "lch": [], "bch": [], "hc": [] },
     ]},
 ];
 
