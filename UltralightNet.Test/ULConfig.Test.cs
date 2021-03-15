f609f38e4b63b557885f2a8bf90e2e22add0e046
SupinePandora43 <supinepandoraggg@gmail.com>
2021-03-15 22:58:19 +0500

    more ULConfig tests

--- UltralightNet.Test/ULConfig.Test.cs
+++ UltralightNet.Test/ULConfig.Test.cs
@@ -131,5 +131,69 @@ namespace UltralightNet.Test
 			config.ForceRepaint = false;
 			Assert.False(config.ForceRepaint);
 		}
+		[Fact]
+		public void AnimationTimerDelayTest()
+		{
+			config.AnimationTimerDelay = 1.0 / 60.0;
+			Assert.Equal(1.0 / 60.0, config.AnimationTimerDelay);
+			config.AnimationTimerDelay = 1.0 / 120.0;
+			Assert.Equal(1.0 / 120.0, config.AnimationTimerDelay);
+		}
+		[Fact]
+		public void ScrollTimerDelayTest()
+		{
+			config.ScrollTimerDelay = 1.0 / 60.0;
+			Assert.Equal(1.0 / 60.0, config.ScrollTimerDelay);
+			config.ScrollTimerDelay = 1.0 / 120.0;
+			Assert.Equal(1.0 / 120.0, config.ScrollTimerDelay);
+		}
+		[Fact]
+		public void RecycleDelayTest()
+		{
+			config.RecycleDelay = 4.0;
+			Assert.Equal(4.0, config.RecycleDelay);
+			config.RecycleDelay = 1.0;
+			Assert.Equal(1.0, config.RecycleDelay);
+		}
+		[Fact]
+		public void MemoryCacheSizeTest()
+		{
+			config.MemoryCacheSize = 64u * 1024u * 1024u;
+			Assert.Equal(64u * 1024u * 1024u, config.MemoryCacheSize);
+			config.MemoryCacheSize = 128u * 1024u * 1024u;
+			Assert.Equal(128u * 1024u * 1024u, config.MemoryCacheSize);
+		}
+		[Fact]
+		public void PageCacheSizeTest()
+		{
+			config.PageCacheSize = 0u;
+			Assert.Equal(0u, config.PageCacheSize);
+			config.PageCacheSize = 5u;
+			Assert.Equal(5u, config.PageCacheSize);
+		}
+		[Fact]
+		public void OverrideRAMSize()
+		{
+			config.OverrideRAMSize = 0u;
+			Assert.Equal(0u, config.OverrideRAMSize);
+			config.OverrideRAMSize = 1024u;
+			Assert.Equal(1024u, config.OverrideRAMSize);
+		}
+		[Fact]
+		public void MinLargeHeapSizeTest()
+		{
+			config.MinLargeHeapSize = 32u * 1024u * 1024u;
+			Assert.Equal(32u * 1024u * 1024u, config.MinLargeHeapSize);
+			config.MinLargeHeapSize = 8u * 1024u * 1024u;
+			Assert.Equal(8u * 1024u * 1024u, config.MinLargeHeapSize);
+		}
+		[Fact]
+		public void MinSmallHeapSizeTest()
+		{
+			config.MinSmallHeapSize = 1u * 1024u * 1024u;
+			Assert.Equal(1u * 1024u * 1024u, config.MinSmallHeapSize);
+			config.MinSmallHeapSize = 512u * 1024u;
+			Assert.Equal(512u * 1024u, config.MinSmallHeapSize);
+		}
 	}
 }
