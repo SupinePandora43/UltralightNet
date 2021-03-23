643ab442a7da80dec88fbc1371da2f80387204ab
SupinePandora43 <supinepandoraggg@gmail.com>
2021-03-23 19:08:12 +0500

    ULSurfaceDefinitionUnlockPixelsCallback

---
+++ UltralightNet/Callbacks/ULSurfaceDefinitionUnlockPixelsCallback.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace UltralightNet
+{
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	public delegate void ULSurfaceDefinitionUnlockPixelsCallback(IntPtr user_data);
+}
