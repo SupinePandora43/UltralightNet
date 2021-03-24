46cedc51919129b666dcfe66cf7beeb742f1734c
SupinePandora43 <supinepandoraggg@gmail.com>
2021-03-24 14:12:55 +0500

    done long long

--- UltralightNet/Callbacks/ULFileSystemReadFromFileCallback.cs
+++ UltralightNet/Callbacks/ULFileSystemReadFromFileCallback.cs
@@ -2,7 +2,6 @@ using System.Runtime.InteropServices;
 
 namespace UltralightNet
 {
-	//todo: long long
 	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
 	public delegate long ULFileSystemReadFromFileCallback(
 		int handle,
