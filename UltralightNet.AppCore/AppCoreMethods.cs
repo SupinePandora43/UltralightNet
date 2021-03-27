f86cbb9c3c4ca37c7a2bad94f14f3f0458b27b94
SupinePandora43 <supinepandoraggg@gmail.com>
2021-03-27 13:31:16 +0500

    AppCore: * ULSettings.cs * format csproj * add supression to AppCoreMethods class * make AppCoreMethods partial

--- UltralightNet.AppCore/AppCoreMethods.cs
+++ UltralightNet.AppCore/AppCoreMethods.cs
@@ -2,7 +2,9 @@ using System.Runtime.InteropServices;
 
 namespace UltralightNet.AppCore
 {
-	public static class AppCoreMethods
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1401:P/Invokes should not be visible", Justification = "<Pending>")]
+	public static partial class AppCoreMethods
 	{
 		static AppCoreMethods() => Methods.Preload();
 
