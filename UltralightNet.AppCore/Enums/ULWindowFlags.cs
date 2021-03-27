4403f77a624309cbc7ef376fd4b1c0fa62a6763d
SupinePandora43 <supinepandoraggg@gmail.com>
2021-03-27 13:23:02 +0500

    ULWindowFlags enum

---
+++ UltralightNet.AppCore/Enums/ULWindowFlags.cs
@@ -0,0 +1,10 @@
+namespace UltralightNet.AppCore
+{
+	public enum ULWindowFlags
+	{
+		kWindowFlags_Borderless = 1 << 0,
+		kWindowFlags_Titled = 1 << 1,
+		kWindowFlags_Resizable = 1 << 2,
+		kWindowFlags_Maximizable = 1 << 3,
+	}
+}
