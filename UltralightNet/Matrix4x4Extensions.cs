6f49351eb2b25dcef2210a1f691d60349c572200
SupinePandora43 <supinepandoraggg@gmail.com>
2021-04-02 13:43:30 +0500

    fix

--- UltralightNet/Matrix4x4Extensions.cs
+++ UltralightNet/Matrix4x4Extensions.cs
@@ -5,8 +5,8 @@ namespace UltralightNet
 {
 	public static partial class Methods
 	{
-		[GeneratedDllImport("Ultralight")]
-		public static partial Matrix4x4 ulApplyProjection(Matrix4x4 transform, float viewport_width, float viewport_height, [MarshalAs(UnmanagedType.I1)] bool flip_y);
+		[DllImport("Ultralight")]
+		public static extern Matrix4x4 ulApplyProjection(Matrix4x4 transform, float viewport_width, float viewport_height, [MarshalAs(UnmanagedType.I1)] bool flip_y);
 	}
 
 	public static class Matrix4x4Extensions
