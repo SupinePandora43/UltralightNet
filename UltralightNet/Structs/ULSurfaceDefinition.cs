ef817db561be8b574bfd65548a56c67691f0a765
SupinePandora43 <supinepandoraggg@gmail.com>
2021-04-09 19:55:36 +0500

    fix ULSurfaceDefinition naming

--- UltralightNet/Structs/ULSurfaceDefinition.cs
+++ UltralightNet/Structs/ULSurfaceDefinition.cs
@@ -2,14 +2,14 @@ namespace UltralightNet
 {
 	public struct ULSurfaceDefinition
 	{
-		public ULSurfaceDefinitionCreateCallback create;
-		public ULSurfaceDefinitionDestroyCallback destroy;
-		public ULSurfaceDefinitionGetWidthCallback get_width;
-		public ULSurfaceDefinitionGetHeightCallback get_height;
-		public ULSurfaceDefinitionGetRowBytesCallback get_row_bytes;
-		public ULSurfaceDefinitionGetSizeCallback get_size;
-		public ULSurfaceDefinitionLockPixelsCallback lock_pixels;
-		public ULSurfaceDefinitionUnlockPixelsCallback unlock_pixels;
-		public ULSurfaceDefinitionResizeCallback resize;
+		public ULSurfaceDefinitionCreateCallback Create;
+		public ULSurfaceDefinitionDestroyCallback Destroy;
+		public ULSurfaceDefinitionGetWidthCallback GetWidth;
+		public ULSurfaceDefinitionGetHeightCallback GetHeight;
+		public ULSurfaceDefinitionGetRowBytesCallback GetRowBytes;
+		public ULSurfaceDefinitionGetSizeCallback GetSize;
+		public ULSurfaceDefinitionLockPixelsCallback LockPixels;
+		public ULSurfaceDefinitionUnlockPixelsCallback UnlockPixels;
+		public ULSurfaceDefinitionResizeCallback Resize;
 	}
 }
