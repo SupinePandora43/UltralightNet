eaa1b3cbded74f179db32fdfa7ecd29a2e4a0925
SupinePandora43 <supinepandoraggg@gmail.com>
2021-05-31 22:00:17 +0500

    expose delegate pointer types

--- UltralightNet/Structs/ULClipboard.cs
+++ UltralightNet/Structs/ULClipboard.cs
@@ -1,9 +1,22 @@
+using System.Runtime.InteropServices;
+
 namespace UltralightNet
 {
+	[StructLayout(LayoutKind.Sequential)]
 	public struct ULClipboard
 	{
 		public ULClipboardClearCallback Clear;
 		public ULClipboardReadPlainTextCallback ReadPlainText;
-		public ULClipboardWritePlainTextCallback WritePlainText;
+		public ULClipboardWritePlainTextCallback__PInvoke__ _WritePlainText;
+
+		public ULClipboardWritePlainTextCallback WritePlainText { set { unsafe { _WritePlainText = (text) => value(ULStringMarshaler.NativeToManaged(text)); } } }
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public unsafe struct _ULClipboard
+	{
+		public delegate* unmanaged[Cdecl]<void> Clear;
+		public delegate* unmanaged[Cdecl]<void*, void> ReadPlainText;
+		public delegate* unmanaged[Cdecl]<ULStringMarshaler.ULStringPTR*, void> WritePlainText;
 	}
 }
