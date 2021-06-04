e1aef9b57cec041681a6533f57b13216aed7395e
SupinePandora43 <supinepandoraggg@gmail.com>
2021-06-04 13:26:31 +0500

    benchmark

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,4 +1,6 @@
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Running;
 using System;
 using System.Runtime.InteropServices;
@@ -11,46 +13,14 @@ namespace Benchmarks
         static void Main(string[] args)
         {
 			var summary = BenchmarkRunner.Run<MyBenchmark>();
+			//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
 		}
 	}
-
 	public unsafe class MyBenchmark
 	{
-		private ULString* str;
-
 		public MyBenchmark()
 		{
-			str = (ULString*)Methods.ulCreateStringUTF16("тесто", 5);
-		}
 
-		[Benchmark]
-		public string First() // wins
-		{
-			return ULString.NativeToManaged(str);
-		}
-
-		public static unsafe string NativeToManaged2(ULString* ulString)
-		{
-			ULString ported = *ulString;
-			return new((char*)ported.data, 0, (int)ported.length);
-		}
-
-		[Benchmark]
-		public string Second()
-		{
-			return NativeToManaged2(str);
-		}
-
-		[Benchmark]
-		public string MF() // wins
-		{
-			// String::.ctor(Char*, int, int)
-			return ULString.NativeToManaged(str);
-		}
-		[Benchmark]
-		public string MM()
-		{
-			return Marshal.PtrToStringUni((IntPtr)str->data, (int)str->length);
 		}
 	}
 }
