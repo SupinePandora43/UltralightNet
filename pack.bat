94d0026a829f357d1f6f7a82c0cb4ee64ea42310
SupinePandora43 <supinepandoraggg@gmail.com>
2021-06-14 11:49:42 +0500

    use Release

--- pack.bat
+++ pack.bat
@@ -1,4 +1,4 @@
-dotnet pack UltralightNet.Binaries
-dotnet pack UltralightNet
-dotnet pack UltralightNet.AppCore
-dotnet pack UltralightNet.Resources
+dotnet pack UltralightNet.Binaries -c Release
+dotnet pack UltralightNet -c Release
+dotnet pack UltralightNet.AppCore -c Release
+dotnet pack UltralightNet.Resources -c Release
