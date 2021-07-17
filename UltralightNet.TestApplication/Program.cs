b50d643637a35a0b439c44d27b416e842d763083
SupinePandora43 <supinepandoraggg@gmail.com>
2021-07-17 18:14:49 +0500

    manually load libraries

--- UltralightNet.TestApplication/Program.cs
+++ UltralightNet.TestApplication/Program.cs
@@ -23,7 +23,18 @@ namespace UltralightNetTestApplication
 			//IntPtr view = ulCreateView(renderer.Ptr, 256, 256, 0, IntPtr.Zero, 0);
 			//View view = new(renderer, 512, 512);
 
-
+			// load order
+			if(OperatingSystem.IsLinux()){
+				NativeLibrary.Load("libgio-2.0.so");
+				NativeLibrary.Load("libglib-2.0.so");
+				NativeLibrary.Load("libgmodule-2.0.so");
+				NativeLibrary.Load("libgobject-2.0.so");
+				NativeLibrary.Load("libgthread-2.0.so");
+				NativeLibrary.Load("libpng12.so.0");
+				NativeLibrary.Load("libffi.so.6");
+				NativeLibrary.Load("libgstreamer-full-1.0.so");
+				NativeLibrary.Load("UltralightCore");
+			}
 			unsafe {
 				ULString* str = (ULString*)Methods.ulCreateString("test123тест123");
 				Console.WriteLine(str->ToManaged());
