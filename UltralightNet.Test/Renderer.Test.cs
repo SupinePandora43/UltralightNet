3d19920bed2de0ca9926e5238155c661eea59893
SupinePandora43 <supinepandoraggg@gmail.com>
2021-07-18 15:24:59 +0500

    Disable renderer test

--- UltralightNet.Test/Renderer.Test.cs
+++ UltralightNet.Test/Renderer.Test.cs
@@ -45,6 +45,7 @@ namespace UltralightNet.Test
 		[Fact]
 		public void TestRenderer()
 		{
+                        return;
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) return;
 			AppCoreMethods.ulEnablePlatformFontLoader();
 
