da72244be5c3595469cea1e0a4b9f4028095a577
SupinePandora43 <supinepandoraggg@gmail.com>
2021-07-19 08:00:32 +0500

    load by absolute path

--- UltralightNet.TestApplication/Program.cs
+++ UltralightNet.TestApplication/Program.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UltralightNet;
 using UltralightNet.AppCore;
 using System;
@@ -25,17 +26,23 @@ namespace UltralightNetTestApplication
 
 			// load order
 			if(OperatingSystem.IsLinux()){
-				NativeLibrary.Load("libgio-2.0.so");
-				NativeLibrary.Load("libglib-2.0.so");
-				NativeLibrary.Load("libgmodule-2.0.so");
-				NativeLibrary.Load("libgobject-2.0.so");
-				NativeLibrary.Load("libgthread-2.0.so");
-				NativeLibrary.Load("libpng12.so.0");
-				NativeLibrary.Load("libffi.so.6");
-				NativeLibrary.Load("libgstreamer-full-1.0.so");
-				NativeLibrary.Load("UltralightCore");
+				//NativeLibrary.Load("libgio-2.0.so");
+				//NativeLibrary.Load("libglib-2.0.so");
+				//NativeLibrary.Load("libgmodule-2.0.so");
+				//NativeLibrary.Load("libgobject-2.0.so");
+				//NativeLibrary.Load("libgthread-2.0.so");
+				//NativeLibrary.Load("/usr/lib64/ld-linux-x86-64.so.2");
+				//NativeLibrary.Load("libpng12.so.0");
+				//NativeLibrary.Load("libpng16.so.16");
+				//NativeLibrary.Load("libffi.so.6");
+				//NativeLibrary.Load("./libgstreamer-full-1.0.so");
+				//NativeLibrary.Load("UltralightCore");
+				//string absoluteAssemblyLocationDir = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+				//string absoluteRuntimeNativesDir = Path.Combine(absoluteAssemblyLocationDir, "runtimes", "linux-x64", "native");
+				//NativeLibrary.Load(Path.Combine(absoluteRuntimeNativesDir, "libgstreamer-full-1.0.so"));
 			}
 			unsafe {
+				Console.WriteLine("test123тест123");
 				ULString* str = (ULString*)Methods.ulCreateString("test123тест123");
 				Console.WriteLine(str->ToManaged());
 				//Console.WriteLine(Marshal.PtrToStringUni(Methods.ulStringGetData((IntPtr)str), (int)Methods.ulStringGetLength((IntPtr)str)));
