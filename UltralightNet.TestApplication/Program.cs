64431183c52d8ea1c711139d51e16c8947755764
SupinePandora43 <supinepandoraggg@gmail.com>
2021-07-26 18:02:12 +0500

    testapp

--- UltralightNet.TestApplication/Program.cs
+++ UltralightNet.TestApplication/Program.cs
@@ -2,6 +2,7 @@ using System.IO;
 using UltralightNet;
 using UltralightNet.AppCore;
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace UltralightNetTestApplication
@@ -10,54 +11,67 @@ namespace UltralightNetTestApplication
 	{
 		static void Main()
 		{
-			//ULConfig config = new();
-			//Renderer renderer = new(config);
+			ULPlatform.SetLogger(new ULLogger(){LogMessage=(level, message)=>Console.WriteLine(message)});
+			
+			ULFileSystem fs = new();
 
-			//Session session = Session.DefaultSession(renderer);
-			//Session session = new(renderer, false, "asd");
+			Dictionary<int, FileStream> files = new();
+			int last = 0;
 
-			//AppCoreMethods.ulEnableDefaultLogger("./log.txt");
-			//AppCoreMethods.ulEnablePlatformFileSystem("./");
-			//AppCoreMethods.ulEnablePlatformFontLoader();
+			string dir = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+			Console.WriteLine(dir);
+			fs.FileExists = (path) =>
+			{
+				Console.WriteLine($"FileExists {path}");
+				return File.Exists(Path.Combine(dir, path));
+			};
 
-			//Ultralight.View view = new(renderer.Ptr, 512, 512, false, session.Ptr);
-			//IntPtr view = ulCreateView(renderer.Ptr, 256, 256, 0, IntPtr.Zero, 0);
-			//View view = new(renderer, 512, 512);
+			fs.OpenFile = (path, for_writing) =>
+			{
+				Console.WriteLine($"OpenFile {path}");
+				FileStream f = File.Open(Path.Combine(dir, path), FileMode.Open, for_writing ? FileAccess.ReadWrite : FileAccess.Read);
+				int id = last++;
+				files[id] = f;
+				return id;
+			};
 
-			// load order
-			if(OperatingSystem.IsLinux()){
-				//NativeLibrary.Load("libgio-2.0.so");
-				//NativeLibrary.Load("libglib-2.0.so");
-				//NativeLibrary.Load("libgmodule-2.0.so");
-				//NativeLibrary.Load("libgobject-2.0.so");
-				//NativeLibrary.Load("libgthread-2.0.so");
-				//NativeLibrary.Load("/usr/lib64/ld-linux-x86-64.so.2");
-				//NativeLibrary.Load("libpng12.so.0");
-				//NativeLibrary.Load("libpng16.so.16");
-				//NativeLibrary.Load("libffi.so.6");
-				//NativeLibrary.Load("./libgstreamer-full-1.0.so");
-				//NativeLibrary.Load("UltralightCore");
-				//string absoluteAssemblyLocationDir = Path.GetDirectoryName(typeof(Program).Assembly.Location);
-				//string absoluteRuntimeNativesDir = Path.Combine(absoluteAssemblyLocationDir, "runtimes", "linux-x64", "native");
-				//NativeLibrary.Load(Path.Combine(absoluteRuntimeNativesDir, "libgstreamer-full-1.0.so"));
-			}
-			unsafe {
-				Console.WriteLine("test123тест123");
-				ULStringGeneratedDllImportMarshaler marshaler = new("test123тест123");
-				Console.WriteLine(marshaler.ToManaged());
-				marshaler.FreeNative();
-				ULString* str = (ULString*)Methods.ulCreateStringUTF16("test123тест123", (uint)"test123тест123".Length);
-				Console.WriteLine(str->ToManaged());
-				//Console.WriteLine(Marshal.PtrToStringUni(Methods.ulStringGetData((IntPtr)str), (int)Methods.ulStringGetLength((IntPtr)str)));
-				Console.WriteLine(Marshal.PtrToStringUni((IntPtr)str->data, (int)str->length));
-			}
+			fs.GetFileSize = (int id, out long result) =>
+			{
+				Console.WriteLine($"GetFileSize {id}");
+				result = files[id].Length;
+				return true;
+			};
+
+			fs.CloseFile = (id) =>
+			{
+				Console.WriteLine($"CloseFile {id}");
+				files[id].Close();
+				files[id] = null;
+			};
+
+			fs.GetFileMimeType = (string path, out string result) =>
+			{
+				Console.WriteLine($"GetFileMimeType {path}");
+				result = "";
+				return true;
+			};
+
+			fs.ReadFromFile = (id, data, length) =>
+			{
+				Console.WriteLine($"ReadFromFile {id} {data.Length} {length}");
+				return (long)files[id].Read(data); ;
+			};
+
+			//AppCoreMethods.ulEnablePlatformFileSystem(Path.GetDirectoryName(typeof(Program).Assembly.Location));
+			ULPlatform.SetFileSystem(fs);
+
 			AppCoreMethods.ulEnablePlatformFontLoader();
-			AppCoreMethods.ulEnableDefaultLogger("./ullog.txt");
-			AppCoreMethods.ulEnablePlatformFileSystem(Path.GetDirectoryName(typeof(Program).Assembly.Location));
-			Console.WriteLine("creating renderer");
+
 			ULConfig cfg = new();
+
 			Renderer renderer = new(cfg);
-			Console.WriteLine("done");
+
+			View view = new(renderer, 512, 512);
 		}
 	}
 }
