915bccdf01963dc5d0761850ed8c02090e2a8c51
SupinePandora43 <supinepandoraggg@gmail.com>
2021-11-14 20:22:10 +0500

    Remove stale benchmarks

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -29,36 +29,6 @@ namespace Benchmarks
 			AppCoreMethods.ulEnablePlatformFontLoader();
 			renderer = ULPlatform.CreateRenderer();
 			view = renderer.CreateView(512, 512);
-
-			return;
-
-			bool loaded = false;
-
-			view.OnFinishLoading += (_, _, _) => loaded = true;
-
-			view.URL = "https://github.com";
-
-			while (!loaded)
-			{
-				renderer.Update();
-				Thread.Sleep(100);
-			}
-
-			renderer.Render();
-		}
-
-		[Benchmark]
-		public void Manually()
-		{
-			ULScrollEvent @event = new() { type = ULScrollEventType.ByPixel, deltaX = 0, deltaY = 0 };
-			view.FireScrollEvent(@event);
-		}
-
-		[Benchmark]
-		public void Constructor()
-		{
-			ULScrollEvent @event = new(ULScrollEventType.ByPixel, 0, 0);
-			view.FireScrollEvent(@event);
 		}
 
 		~MyBenchmark()
