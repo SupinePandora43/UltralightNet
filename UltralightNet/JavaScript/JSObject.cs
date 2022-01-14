214898c524d9fb92a5b0c64ff69e3d2d0ba7b7af
SupinePandora43 <supinepandoraggg@gmail.com>
2022-01-14 13:30:23 +0500

    update

--- UltralightNet/JavaScript/JSObject.cs
+++ UltralightNet/JavaScript/JSObject.cs
@@ -23,7 +23,44 @@ namespace UltralightNet
 		public static extern bool JSClassSetPrivate(void* jsClass, void* data);
 
 		[DllImport("WebCore")]
+		public static extern void* JSObjectMake(void* context, void* jsClass, void* data);
+
+		[DllImport("WebCore")]
 		public static extern void* JSObjectMakeFunctionWithCallback(void* context, void* name, delegate* unmanaged[Cdecl]<void*, void*, void*, nuint, void**, void**, void*> func);
+
+		[DllImport("WebCore")]
+		public static extern void* JSObjectMakeConstructor(void* context, void* jsClass, delegate* unmanaged[Cdecl]<void*, void*, nuint, void**, void**, void*> func);
+
+		[DllImport("WebCore")]
+		public static extern void* JSObjectMakeArray(void* context, nuint argumentCount, void** arguments, void** exception);
+
+		[DllImport("WebCore")]
+		public static extern void* JSObjectMakeDate(void* context, nuint argumentCount, void** arguments, void** exception);
+
+		[DllImport("WebCore")]
+		public static extern void* JSObjectMakeError(void* context, nuint argumentCount, void** arguments, void** exception);
+
+		[DllImport("WebCore")]
+		public static extern void* JSObjectMakeRegExp(void* context, nuint argumentCount, void** arguments, void** exception);
+
+		[DllImport("WebCore")]
+		public static extern void* JSObjectMakeDeferredPromise(void* context, void* resolve, void* reject, void** exception);
+
+		[DllImport("WebCore")]
+		public static extern void* JSObjectMakeFunction(void* context, void* name, uint parameterCount, void** parameterNames, void* body, void* sourceURL, int startingLineNumber, void** exception);
+
+		[DllImport("WebCore")]
+		public static extern void* JSObjectGetPrototype(void* context, void* jsObject);
+
+		[DllImport("WebCore")]
+		public static extern void JSObjectSetPrototype(void* context, void* jsObject, void* jsValue);
+
+		[GeneratedDllImport("WebCore")]
+		[return: MarshalAs(UnmanagedType.I1)]
+		public static extern bool JSObjectHasProperty(void* jsClass, void* jsObject, void* propertyName);
+
+		[DllImport("WebCore")]
+		public static extern void* JSObjectGetProperty(void* context, void* jsObject, void* propertyName, void** exception);
 
 		[DllImport("WebCore")]
 		public static extern void JSObjectSetProperty(void* context, void* jsObject, void* propertyName, void* value, JSPropertyAttributes attributes = JSPropertyAttributes.None, void** exception = null);
