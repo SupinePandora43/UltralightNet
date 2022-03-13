007382e919e6506c5748b97a498ffe0f8fba65fc
SupinePandora43 <supinepandoraggg@gmail.com>
2022-03-13 10:47:46 +0500

    JSString overhaul

---
+++ UltralightNet.Test/JSString.Test.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace UltralightNet.Test;
+
+public unsafe class JSStringTest
+{
+	public static IEnumerable<object[]> GetTestStrings()
+	{
+		yield return new JSString[] { "TEST" };
+		yield return new JSString[] { "ТЕСТ" };
+		yield return new JSString[] { "" };
+	}
+
+	[Theory]
+	[MemberData(nameof(GetTestStrings))]
+	public void Clone(JSString s)
+	{
+		string m = (string)s;
+		var r = s.Clone();
+		Assert.Equal(s, r);
+		Assert.Equal(m, (string)r);
+		Assert.True(JSString.ReferenceEquals(s, r));
+		s.Dispose();
+		Assert.Equal(m, (string)r);
+		r.Dispose();
+	}
+
+	[Theory]
+	[MemberData(nameof(GetTestStrings))]
+	public void Equality(JSString s1)
+	{
+		JSString s2 = new((string)s1);
+		Assert.True(s1.Equals(s2));
+		Assert.True(s1.Equals((object)s2));
+		Assert.True(s1 == s2);
+		Assert.False(s1 != s2);
+		Assert.Equal(s1, s2);
+		Assert.Equal((object)s1, (object)s2);
+		Assert.False(JSString.ReferenceEquals(s1, s2));
+		Assert.False(s1 == null);
+	}
+}
--- UltralightNet/JavaScript/JSString.cs
+++ UltralightNet/JavaScript/JSString.cs
@@ -1,6 +1,7 @@
 // JSStringRef.h
 
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace UltralightNet
@@ -14,9 +15,11 @@ namespace UltralightNet
 		[DllImport("WebCore")]
 		public static extern void* JSStringCreateWithUTF8CString(byte* characters);
 
+		/// <summary>Increases ref count</summary>
 		[DllImport("WebCore")]
 		public static extern void* JSStringRetain(void* @string);
 
+		/// <summary>Decreases ref count</summary>
 		[DllImport("WebCore")]
 		public static extern void JSStringRelease(void* @string);
 
@@ -41,7 +44,8 @@ namespace UltralightNet
 		public static partial bool JSStringIsEqualToUTF8CString(void* str, byte* characters);
 	}
 
-	public unsafe class JSString : IDisposable, IEquatable<JSString>
+	[DebuggerDisplay("{ToString(),raw}")]
+	public unsafe sealed class JSString : IDisposable, IEquatable<JSString>, ICloneable
 	{
 		private readonly void* handle;
 		private readonly bool dispose = true;
@@ -62,35 +66,44 @@ namespace UltralightNet
 
 		public void* Handle => handle;
 
-		public JSString Retain() => new(JavaScriptMethods.JSStringRetain(Handle), true);
+		public JSString Clone() => new(JavaScriptMethods.JSStringRetain(Handle), true);
+		object ICloneable.Clone() => Clone();
 
 		public nuint Length => JavaScriptMethods.JSStringGetLength(Handle);
 
-		public ReadOnlySpan<ushort> UTF16Data => new(JavaScriptMethods.JSStringGetCharactersPtr(Handle), (int)Length);
+		public ReadOnlySpan<ushort> UTF16Data => new(JavaScriptMethods.JSStringGetCharactersPtr(Handle), (int)Length); // INTEROPTODO: INT64
 		public ushort* UTF16DataRaw => JavaScriptMethods.JSStringGetCharactersPtr(Handle);
 
 		public nuint MaximumUTF8CStringSize => JavaScriptMethods.JSStringGetMaximumUTF8CStringSize(Handle);
 
 		public nuint GetUTF8(byte* buffer, nuint bufferSize) => JavaScriptMethods.JSStringGetUTF8CString(Handle, buffer, bufferSize);
-		public nuint GetUTF8(Span<byte> buffer) { fixed (byte* bufferPtr = buffer) { return JavaScriptMethods.JSStringGetUTF8CString(Handle, bufferPtr, (nuint)buffer.Length); } }
-
-#nullable enable
+		public nuint GetUTF8(Span<byte> buffer) { fixed (byte* bufferPtr = buffer) { return JavaScriptMethods.JSStringGetUTF8CString(Handle, bufferPtr, (nuint)buffer.Length); } } // INTEROPTODO: INT64
 
 		public override string? ToString() => new((char*)UTF16DataRaw, 0, (int)Length);
 
-		public override bool Equals(object? other) => Equals(other as JSString);
+		public override bool Equals(object? other) => Equals(other is not null and string ? (JSString)((string)other) : other as JSString);
 		public bool Equals(JSString? other)
 		{
 			if (other is null) return false;
 			return JavaScriptMethods.JSStringIsEqual(Handle, other.Handle);
 		}
+		public static bool operator ==(JSString left, JSString right) => left.Equals(right);
+		public static bool operator !=(JSString left, JSString right) => !(left == right);
 
-#nullable disable
+		public static bool ReferenceEquals(JSString s1, JSString s2) => s1.UTF16DataRaw == s2.UTF16DataRaw && s1.Length == s2.Length;
+
+		public override int GetHashCode()
+		{
+			HashCode hash = new();
+			hash.AddBytes(new ReadOnlySpan<byte>((byte*)UTF16DataRaw, (int)(Length * 2))); // INTEROPTODO: INT64
+			return hash.ToHashCode();
+		}
 
 		public bool Equals(byte* other) => JavaScriptMethods.JSStringIsEqualToUTF8CString(Handle, other);
 		public bool Equals(ReadOnlySpan<byte> other) { fixed (byte* bytes = other) { return Equals(bytes); } }
 
-		public static implicit operator JSString(string obj) => new(obj);
+		public static implicit operator JSString(string obj) => new(obj ?? string.Empty);
+		public static explicit operator string(JSString str) => str?.ToString()!;
 
 		~JSString() => Dispose();
 
