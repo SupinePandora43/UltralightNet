576d73ea198d6da0d33b85af10ab280c2da9cea5
SupinePandora43 <supinepandoraggg@gmail.com>
2022-03-26 14:02:10 +0500

    Make RenderTarget a [BlittableType] Use dotnet naming conventions

--- src/UltralightNet/RenderTarget.cs
+++ src/UltralightNet/RenderTarget.cs
@@ -1,68 +1,31 @@
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace UltralightNet
 {
 	/// <summary>Rendering details for a View, to be used with your own GPUDriver</summary>
-	[NativeMarshalling(typeof(RenderTargetNative))]
+	[BlittableType]
 	public struct RenderTarget
 	{
+		private byte _IsEmpty;
 		/// <summary>Whether this target is empty (null texture)</summary>
-		public bool is_empty;
+		public bool IsEmpty { get => Unsafe.As<byte, bool>(ref _IsEmpty); set => _IsEmpty = Unsafe.As<bool, byte>(ref value); }
 		/// <summary>The viewport width (in device coordinates).</summary>
-		public uint width;
+		public uint Width;
 		/// <summary>The viewport height (in device coordinates).</summary>
-		public uint height;
+		public uint Height;
 		/// <summary>The GPUDriver-specific texture ID (you should bind the texture using your implementation of GPUDriver::BindTexture before drawing a quad).</summary>
-		public uint texture_id;
+		public uint TextureId;
 		/// <summary>The texture width (in pixels). This may be padded.</summary>
-		public uint texture_width;
+		public uint TextureWidth;
 		/// <summary>The texture height (in pixels). This may be padded.</summary>
-		public uint texture_height;
+		public uint TextureHeight;
+		private byte _TextureFormat;
 		/// <summary>The pixel format of the texture.</summary>
-		public ULBitmapFormat texture_format;
+		public ULBitmapFormat TextureFormat { get => Unsafe.As<byte, ULBitmapFormat>(ref _TextureFormat); set => _TextureFormat = Unsafe.As<ULBitmapFormat, byte>(ref value); }
 		/// <summary>UV coordinates of the texture (this is needed because the texture may be padded).</summary>
-		public ULRect uv_coords;
+		public ULRect UV;
 		/// <summary>The GPUDriver-specific render buffer ID.</summary>
-		public uint render_buffer_id;
-	}
-
-	[BlittableType]
-	internal struct RenderTargetNative
-	{
-		public byte is_empty;
-		public uint width;
-		public uint height;
-		public uint texture_id;
-		public uint texture_width;
-		public uint texture_height;
-		public int texture_format;
-		public ULRect uv_coords;
-		public uint render_buffer_id;
-
-		public RenderTargetNative(RenderTarget rt)
-		{
-			is_empty = rt.is_empty ? (byte)1 : (byte)0;
-			width = rt.width;
-			height = rt.height;
-			texture_id = rt.texture_id;
-			texture_width = rt.texture_width;
-			texture_height = rt.texture_height;
-			texture_format = (int)rt.texture_format;
-			uv_coords = rt.uv_coords;
-			render_buffer_id = rt.render_buffer_id;
-		}
-
-		public RenderTarget ToManaged() => new()
-		{
-			is_empty = is_empty != 0,
-			width = width,
-			height = height,
-			texture_id = texture_id,
-			texture_height = texture_height,
-			texture_width = texture_width,
-			texture_format = (ULBitmapFormat)texture_format,
-			uv_coords = uv_coords,
-			render_buffer_id = render_buffer_id
-		};
+		public uint RenderBufferID;
 	}
 }
