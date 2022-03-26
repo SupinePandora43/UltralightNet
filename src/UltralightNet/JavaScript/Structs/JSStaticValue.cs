67384517021ac074d9763ef37dc34f8d603dff22
SupinePandora43 <supinepandoraggg@gmail.com>
2022-03-26 14:37:06 +0500

    Make JSStaticValue [BlittableType]

--- src/UltralightNet/JavaScript/Structs/JSStaticValue.cs
+++ src/UltralightNet/JavaScript/Structs/JSStaticValue.cs
@@ -1,17 +1,24 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 namespace UltralightNet
 {
+	[BlittableType]
 	public unsafe struct JSStaticValue
 	{
 		public byte* name;
 		public delegate* unmanaged[Cdecl]<void*, void*, void*, void**, void*> getProperty;
 		public delegate* unmanaged[Cdecl]<void*, void*, void*, void*, void**, bool> setProperty;
-		public JSPropertyAttributes attributes;
+		private uint _Attributes;
+		public JSPropertyAttributes Attributes { get => Unsafe.As<uint, JSPropertyAttributes>(ref _Attributes); set => _Attributes = Unsafe.As<JSPropertyAttributes, uint>(ref value); }
 	}
+	[BlittableType]
 	public unsafe struct JSStaticValueEx
 	{
 		public byte* name;
 		public delegate* unmanaged[Cdecl]<void*, void*, void*, void*, void**, void*> getPropertyEx;
 		public delegate* unmanaged[Cdecl]<void*, void*, void*, void*, void*, void**, bool> setPropertyEx;
-		public JSPropertyAttributes attributes;
+		private uint _Attributes;
+		public JSPropertyAttributes Attributes { get => Unsafe.As<uint, JSPropertyAttributes>(ref _Attributes); set => _Attributes = Unsafe.As<JSPropertyAttributes, uint>(ref value); }
 	}
 }
