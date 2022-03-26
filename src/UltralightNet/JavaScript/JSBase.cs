5d7bc2ec00519ef2124bad4f4ae4b7c3753eaef5
SupinePandora43 <supinepandoraggg@gmail.com>
2022-03-26 14:48:06 +0500

    Implement "ThrowUnsupportedConstructor" Add const string LibWebCore

--- src/UltralightNet/JavaScript/JSBase.cs
+++ src/UltralightNet/JavaScript/JSBase.cs
@@ -1,19 +1,24 @@
 // JSBase.h
 
+using System;
 using System.Runtime.InteropServices;
 
 namespace UltralightNet
 {
 	unsafe partial class JavaScriptMethods
 	{
-		[DllImport("WebCore")]
+		public const string LibWebCore = "WebCore";
+		
+		internal static void ThrowUnsupportedConstructor() => throw new NotSupportedException("Creation of this type is disallowed.");
+
+		[DllImport(LibWebCore)]
 		public static extern void* JSEvaluateScript(void* context, void* script, void* thisObject, void* sourceURL, int startingLineNumber, void** exception);
 
-		[GeneratedDllImport("WebCore")]
+		[GeneratedDllImport(LibWebCore)]
 		[return: MarshalAs(UnmanagedType.I1)]
 		public static partial bool JSCheckScriptSyntax(void* context, void* script, void* sourceURL, int startingLineNumber, void** exception);
 
-		[DllImport("WebCore")]
+		[DllImport(LibWebCore)]
 		public static extern void JSGarbageCollect(void* context);
 	}
 }
