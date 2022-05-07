a9737975ff6a0da66a3742b2bf68b1c988c1250b
SupinePandora43 <supinepandoraggg@gmail.com>
2022-05-07 06:20:45 +0500

    Add missing usings

--- src/UltralightNet/Structs/ULGPUState.cs
+++ src/UltralightNet/Structs/ULGPUState.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace UltralightNet
--- src/UltralightNet/Structs/ULVertexBuffer.cs
+++ src/UltralightNet/Structs/ULVertexBuffer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace UltralightNet
