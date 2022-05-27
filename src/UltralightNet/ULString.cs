1a15732caf72ce4c9496d5f15fc3d905073ff6c5
SupinePandora43 <supinepandoraggg@gmail.com>
2022-05-27 20:53:30 +0500

    Fix access violation port to older TFMs

--- src/UltralightNet/ULString.cs
+++ src/UltralightNet/ULString.cs
@@ -24,7 +24,6 @@ public static unsafe partial class Methods
 
 	/// <summary>Create string from UTF-16 buffer.</summary>
 	[LibraryImport(LibUltralight)]
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static partial ULString* ulCreateStringUTF16([MarshalAs(UnmanagedType.LPWStr)] string str, nuint len);
 
 	/// <summary>Create string from UTF-16 buffer.</summary>
@@ -75,33 +74,80 @@ public static unsafe partial class Methods
 	}
 }
 
-[DebuggerDisplay("{ToManaged(),raw}")]
+[DebuggerDisplay("{ToString(),raw}")]
 [StructLayout(LayoutKind.Sequential)]
-public unsafe struct ULString : IDisposable, ICloneable
+public unsafe struct ULString : IDisposable, ICloneable, IEquatable<ULString>
 {
 	public byte* data;
 	public nuint length;
 
 	public ULString(ReadOnlySpan<char> chars)
 	{
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1
 		data = (byte*)NativeMemory.Alloc((length = (nuint)Encoding.UTF8.GetByteCount(chars)) + 1);
 		nuint written = (nuint)Encoding.UTF8.GetBytes(chars, new Span<byte>(data, (int)length)); // INTEROPTODO: INT64
-		data[length = written] = 0;
+#else
+		nuint written = 0;
+		if (chars.Length is not 0)
+			fixed (char* charPtr = chars)
+				written = (nuint)Encoding.UTF8.GetBytes(charPtr, chars.Length, data = (byte*)NativeMemory.Alloc((length = (nuint)Encoding.UTF8.GetByteCount(charPtr, chars.Length)) + 1), (int)length); // INTEROPTODO: INT64
+		else data = (byte*)NativeMemory.Alloc((length = 0) + 1);
+#endif
+		Debug.Assert(written == length);
+		data[length] = 0;
 	}
 	public ULString(ReadOnlySpan<byte> chars)
 	{
 		data = (byte*)NativeMemory.Alloc((length = (nuint)chars.Length) + 1);
 		chars.CopyTo(new Span<byte>(data, (int)length));
+		data[length] = 0;
+	}
+	public ULString()
+	{
+		(data = (byte*)NativeMemory.Alloc(1))
+			[length = 0] = 0;
 	}
 
 	public void Dispose()
 	{
-		if (data is null) return;
-		NativeMemory.Free(data);
-		data = null;
-		length = 0;
+		if (data is not null)
+		{
+			NativeMemory.Free(data);
+			data = null;
+			length = 0; // why? string shouldn't even be accessed after disposal! i just do this to notice problems
+		}
 	}
 
+	public void Assign(ReadOnlySpan<char> newStr)
+	{
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1
+		data = (byte*)NativeMemory.Alloc((length = (nuint)Encoding.UTF8.GetByteCount(newStr)) + 1);
+		nuint written = (nuint)Encoding.UTF8.GetBytes(newStr, new Span<byte>(data, (int)length)); // INTEROPTODO: INT64
+#else
+		nuint written = 0;
+		if(newStr.Length is not 0)
+			fixed (char* charPtr = newStr)
+				written = (nuint)Encoding.UTF8.GetBytes(charPtr, newStr.Length,
+				data = data is not null ?
+					(byte*)NativeMemory.Realloc(data, (length = (nuint)Encoding.UTF8.GetByteCount(charPtr, newStr.Length)) + 1) :
+					(byte*)NativeMemory.Alloc((length = (nuint)Encoding.UTF8.GetByteCount(charPtr, newStr.Length)) + 1), (int)length); // INTEROPTODO: INT64
+		else
+			data = data is not null ?
+				(byte*)NativeMemory.Realloc(data, (length = 0) + 1) :
+				(byte*)NativeMemory.Alloc((length = 0) + 1);
+#endif
+		Debug.Assert(written == length);
+		data[length] = 0;
+	}
+	public void Assign(ReadOnlySpan<byte> newStr)
+	{
+		if (data is not null) data = (byte*)NativeMemory.Realloc(data, (length = (nuint)newStr.Length) + 1);
+		else data = (byte*)NativeMemory.Alloc((length = (nuint)newStr.Length) + 1);
+
+		newStr.CopyTo(new Span<byte>(data, newStr.Length));
+
+		data[length] = 0;
+	}
 	public void Assign(ULString newStr)
 	{
 		if (data is not null) data = (byte*)NativeMemory.Realloc(data, newStr.length + 1);
@@ -111,7 +157,7 @@ public unsafe struct ULString : IDisposable, ICloneable
 
 		Buffer.MemoryCopy(data, newStr.data, length, length);
 
-		data[length] = 0; // just to make sure
+		data[length] = 0;
 	}
 	public void Assign(ULString* newStr)
 	{
@@ -122,13 +168,13 @@ public unsafe struct ULString : IDisposable, ICloneable
 
 		Buffer.MemoryCopy(data, newStr->data, length, length);
 
-		data[length] = 0; // just to make sure
+		data[length] = 0;
 	}
 
 	/// <remarks>it doesn't copy</remarks>
 	public readonly ULString* Allocate()
 	{
-		ULString* str = (ULString*)NativeMemory.Alloc((nuint)sizeof(ULString*));
+		ULString* str = (ULString*)NativeMemory.Alloc((nuint)sizeof(ULString));
 		str->data = data;
 		str->length = length;
 		return str;
@@ -136,20 +182,28 @@ public unsafe struct ULString : IDisposable, ICloneable
 
 	public readonly ULString Clone()
 	{
-		Unsafe.SkipInit(out ULString clone);
-
-		clone.data = (byte*)NativeMemory.Alloc(length + 1);
-		clone.length = length;
+		ULString clone = new()
+		{
+			data = (byte*)NativeMemory.Alloc(length + 1),
+			length = length
+		};
 
 		Buffer.MemoryCopy(data, clone.data, length, length);
 
-		clone.data[length] = 0; // just to make sure
+		clone.data[length] = 0;
 
 		return clone;
 	}
 	readonly object ICloneable.Clone() => Clone();
 
-	public static explicit operator string(ULString str) => // INTEROPTODO: INT64
+	public readonly bool Equals(ULString str) => length == str.length ? (data == str.data ? true : new ReadOnlySpan<byte>(data, (int)length).SequenceEqual(new ReadOnlySpan<byte>(str.data, (int)str.length))) : false;
+	public override readonly bool Equals(object? other) => other is ULString str ? Equals(str) : false;
+
+#if NETSTANDARD2_1 || NETCOREAPP2_1_OR_GREATER
+	public override readonly int GetHashCode() => HashCode.Combine((nuint)data, length);
+#endif
+
+	public static explicit operator string(ULString str) => str.data is null ? string.Empty : // INTEROPTODO: INT64
 #if NETSTANDARD2_1 || NETCOREAPP1_1_OR_GREATER
 		Marshal.PtrToStringUTF8((IntPtr)str.data, (int)str.length);
 #elif NETSTANDARD2_0
@@ -159,6 +213,8 @@ public unsafe struct ULString : IDisposable, ICloneable
 #else
 		Marshal.PtrToStringUTF8((IntPtr)str.data, (int)str.length);
 #endif
+
+	public override readonly string ToString() => (string)this;
 
 	internal static string NativeToManaged(ULString* str) => str is null ? string.Empty : // INTEROPTODO: INT64
 #if NETSTANDARD2_1 || NETCOREAPP1_1_OR_GREATER
@@ -179,7 +235,7 @@ public unsafe struct ULString : IDisposable, ICloneable
 
 		public ToNative(string str)
 		{
-			ulString = new(str);
+			ulString = new(str.AsSpan());
 		}
 
 		public ULString* Value => (ULString*)Unsafe.AsPointer(ref ulString);
