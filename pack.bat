c570870b7fe0885c4421a526447a542e032d756d
Robin Rodricks <robinrodricks7@gmail.com>
2022-06-26 17:55:01 +0530

    Update pack.bat

--- pack.bat
+++ pack.bat
@@ -2,4 +2,3 @@ dotnet pack src\UltralightNet.Binaries\UltralightNet.Binaries.csproj -c Release
 dotnet pack src\UltralightNet.AppCore\UltralightNet.AppCore.csproj -c Release
 dotnet pack src\UltralightNet.Resources\UltralightNet.Resources.csproj -c Release
 dotnet pack src\UltralightNet\UltralightNet.csproj -c Release
-pause
