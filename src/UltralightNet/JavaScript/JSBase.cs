a3ca22460ddb3d9c2caf07bf3ccc60125543df61
SupinePandora43 <supinepandoraggg@gmail.com>
2022-07-10 16:51:13 +0500

    start

--- src/UltralightNet/JavaScript/JSBase.cs
+++ src/UltralightNet/JavaScript/JSBase.cs
@@ -3,22 +3,31 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace UltralightNet
+namespace UltralightNet.JavaScript;
+
+public static unsafe partial class JavaScriptMethods
 {
-	unsafe partial class JavaScriptMethods
+	public const string LibWebCore = "WebCore";
+
+	internal static void ThrowUnsupportedConstructor() => throw new NotSupportedException("Creation of this type is disallowed.");
+
+	[DllImport(LibWebCore)]
+	public static extern JSValueRef JSEvaluateScript(JSContextRef context, JSStringRef script, JSObjectRef thisObject, JSStringRef sourceURL, int startingLineNumber, JSValueRef* exception);
+	public static extern JSValueRef JSEvaluateScript(JSContextRef context, JSStringRef script, JSObjectRef thisObject, JSStringRef sourceURL, int startingLineNumber, out JSValueRef exception)
 	{
-		public const string LibWebCore = "WebCore";
-		
-		internal static void ThrowUnsupportedConstructor() => throw new NotSupportedException("Creation of this type is disallowed.");
-
-		[DllImport(LibWebCore)]
-		public static extern void* JSEvaluateScript(void* context, void* script, void* thisObject, void* sourceURL, int startingLineNumber, void** exception);
-
-		[GeneratedDllImport(LibWebCore)]
-		[return: MarshalAs(UnmanagedType.I1)]
-		public static partial bool JSCheckScriptSyntax(void* context, void* script, void* sourceURL, int startingLineNumber, void** exception);
-
-		[DllImport(LibWebCore)]
-		public static extern void JSGarbageCollect(void* context);
+		fixed (JSValueRef* exceptionPtr)
+			return JSEvaluateScript(context, script, thisObject, sourceURL, startingLineNumber, exceptionPtr);
 	}
+
+	[GeneratedDllImport(LibWebCore)]
+	[return: MarshalAs(UnmanagedType.I1)]
+	public static partial bool JSCheckScriptSyntax(JSContextRef context, JSStringRef script, JSStringRef sourceURL, int startingLineNumber, JSValueRef* exception);
+	public static partial bool JSCheckScriptSyntax(JSContextRef context, JSStringRef script, JSStringRef sourceURL, int startingLineNumber, out JSValueRef exception)
+	{
+		fixed (JSValueRef* exceptionPtr = exception)
+			return JSCheckScriptSyntax(context, script, sourceURL, startingLineNumber, exceptionPtr);
+	}
+
+	[DllImport(LibWebCore)]
+	public static extern void JSGarbageCollect(JSContextRef context);
 }
