a98843e7da93acd27a303c2949d49f3299f9c994
SupinePandora43 <supinepandoraggg@gmail.com>
2023-07-22 13:48:01 +0500

    fix ci

--- regenerate.sh
+++ regenerate.sh
@@ -3,4 +3,5 @@
 dotnet build -c Release -f net7.0 src/UltralightNet
 dotnet build -c Release -f net7.0 src/UltralightNet.AppCore
 cp ./src/UltralightNet/obj/Release/net7.0/generated/Microsoft.Interop.LibraryImportGenerator/Microsoft.Interop.LibraryImportGenerator/LibraryImports.g.cs ./src/UltralightNet/LibraryImportGenerator/LibraryImports.g.cs
+mkdir ./src/UltralightNet.AppCore/LibraryImportGenerator
 cp ./src/UltralightNet.AppCore/obj/Release/net7.0/generated/Microsoft.Interop.LibraryImportGenerator/Microsoft.Interop.LibraryImportGenerator/LibraryImports.g.cs ./src/UltralightNet.AppCore/LibraryImportGenerator/LibraryImports.g.cs
