c5ab556fec3e50b6981fef1b9feb112fb4a5f9a7
SupinePandora43 <supinepandoraggg@gmail.com>
2023-07-30 22:40:09 +0500

    Cleanup the solution

--- Examples/Headless/GettingStarted/Program.cs
+++ Examples/Headless/GettingStarted/Program.cs
@@ -4,53 +4,44 @@ using System.Threading;
 using UltralightNet;
 using UltralightNet.AppCore;
 
-namespace GettingStarted
+// Set Font Loader
+AppCoreMethods.ulEnablePlatformFontLoader();
+
+// Create Renderer
+var cfg = new ULConfig();
+using Renderer renderer = ULPlatform.CreateRenderer(cfg);
+
+// Create View
+using View view = renderer.CreateView(1980, 1024);
+
+// Load URL
+
+bool loaded = false;
+
+view.OnFinishLoading += (frameId, isMainFrame, url) =>
 {
-	class Program
-	{
-		static void Main()
-		{
-			// Set Font Loader
-			AppCoreMethods.ulEnablePlatformFontLoader();
+	loaded = true;
+};
 
-			// Create Renderer
-			var cfg = new ULConfig();
-			Renderer renderer = ULPlatform.CreateRenderer(cfg);
+view.URL = "https://ultralig.ht";
 
-			// Create View
-			View view = renderer.CreateView(1980, 1024);
-
-			// Load URL
-
-			bool loaded = false;
-
-			view.OnFinishLoading += (frameId, isMainFrame, url) =>
-			{
-				loaded = true;
-			};
-
-			view.URL = "https://ultralig.ht";
-
-			// Update Renderer until page is loaded
-			while (!loaded)
-			{
-				renderer.Update();
-				// give time to process network etc.
-				Thread.Sleep(10);
-			}
-
-			// Render
-			renderer.Render();
-
-			// Get Surface
-			ULSurface surface = view.Surface;
-
-			// Get Bitmap
-			ULBitmap bitmap = surface.Bitmap;
-
-			// Save bitmap to png file
-			var path = Path.GetDirectoryName(typeof(Program).Assembly.Location);
-			bitmap.WritePng(Path.Combine(path, "OUTPUT.png"));
-		}
-	}
+// Update Renderer until page is loaded
+while (!loaded)
+{
+	renderer.Update();
+	// give time to process network etc.
+	Thread.Sleep(10);
 }
+
+// Render
+renderer.Render();
+
+// Get Surface
+ULSurface surface = view.Surface ?? throw new Exception("Surface not found, did you perhaps set ViewConfig.IsAccelerated to true?");
+
+// Get Bitmap
+ULBitmap bitmap = surface.Bitmap;
+
+// Save bitmap to png file
+var path = Path.GetDirectoryName(typeof(Program).Assembly.Location)!;
+bitmap.WritePng(Path.Combine(path, "OUTPUT.png"));
